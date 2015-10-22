52c7b34076fab559dc8eb8ef86c05c370774406e
vicfriedman <victoria.m.friedman@gmail.com>
2015-10-22 14:15:47 -0400

    solution

--- js/gameshow.js
+++ js/gameshow.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
     //door one: You have to place your jQuery Selector in THREE places
     //wherever you see `__`
     //You always want to be using a selector on the div with the class "perspective"
-    $("__").flip({
+    $("#door1").flip({
         trigger: 'manual'
     });
     //insert selector below:
-    $("__").click(function(){
+    $("#door1").click(function(){
         //insert selector below:
-        $("__").flip(true);
+        $("#door1").flip(true);
         $("div.perspective").addClass("done");
     });
 
@@ -17,13 +17,13 @@ $(document).ready(function(){
     //wherever you see `__`
     //You always want to be using a selector on the div with the class "perspective"
 
-    $("__").flip({
+    $(".door2").flip({
         trigger: 'manual'
     })
     //insert selector below:
-    $("__").click(function(){
+    $(".door2").click(function(){
         //insert selector below:
-        $("__").flip(true);
+        $(".door2").flip(true);
         $("div.perspective").addClass("done")
     });
 
@@ -31,13 +31,13 @@ $(document).ready(function(){
     //wherever you see `__`
     //You always want to be using a selector on the div with the class "perspective"
 
-    $("__").flip({
+    $(".door-three").flip({
         //insert selector below:
         trigger: 'manual'
     })
-    $("__").click(function(){
+    $(".door-three").click(function(){
         //insert selector below:
-        $("__").flip(true);
+        $(".door-three").flip(true);
         $("div.perspective").addClass("done")
     });
 
