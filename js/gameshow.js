2907c8a1d0d405d499fe712fe26bfd042a574987
nturner2019 <nturner@sch.org>
2016-03-02 13:39:41 +0000

    Done.

--- js/gameshow.js
+++ js/gameshow.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
-    
+
     //door one: You have to place your jQuery Selector in THREE places
     //wherever you see `__`
     //You always want to be using a selector on the div with the class "perspective"
-    $("__").flip({
+    $(".perspective").flip({
         trigger: 'manual'
     });
     //insert selector below:
-    $("__").click(function(){
+    $(".perspective").click(function(){
         //insert selector below:
-        $("__").flip(true);
+        $(".perspective").flip(true);
         $("div.perspective").addClass("done");
         checkWin();
     });
@@ -18,13 +18,13 @@ $(document).ready(function(){
     //wherever you see `__`
     //You always want to be using a selector on the div with the class "perspective"
 
-    $("__").flip({
+    $(".perspective").flip({
         trigger: 'manual'
     })
     //insert selector below:
-    $("__").click(function(){
+    $(".perspective").click(function(){
         //insert selector below:
-        $("__").flip(true);
+        $(".perspective").flip(true);
         $("div.perspective").addClass("done")
         checkWin();
     });
@@ -33,17 +33,17 @@ $(document).ready(function(){
     //wherever you see `__`
     //You always want to be using a selector on the div with the class "perspective"
 
-    $("__").flip({
+    $(".perspective").flip({
         //insert selector below:
         trigger: 'manual'
     })
-    $("__").click(function(){
+    $(".perspective").click(function(){
         //insert selector below:
-        $("__").flip(true);
+        $(".perspective").flip(true);
         $("div.perspective").addClass("done");
         checkWin();
     });
- 
+
     // IGNORE THIS:
     function checkWin() {
       if ($("div.perspective.done").length === $("div.perspective").length) {
